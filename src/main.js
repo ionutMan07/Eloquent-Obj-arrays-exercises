7c2d354c077fcacae207181acf07fe0aebcc8b1e
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-10-06 00:46:45 +0300

    done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // ex.1 solution 1.
 
-// let range = function (first, second, step = 1) {
+// let range = (first, second, step = 1) => {
 //   let result = [];
 //   if (step > 0) {
 //     for (let i = 1; i <= second; i += step) {
@@ -17,7 +17,7 @@
 // console.log(range(1, 10, 2));
 // console.log(range(5, 2, -1));
 
-// let sum = function (array) {
+// let sum = (array) => {
 //   let result = 0;
 //   for (let char of array) {
 //     result += char;
@@ -29,25 +29,25 @@
 
 //ex.1 solution 2
 
-function range(start, end, step = start < end ? 1 : -1) {
-  let array = [];
+// function range(start, end, step = start < end ? 1 : -1) {
+//   let array = [];
 
-  if (step > 0) {
-    for (let i = start; i <= end; i += step) array.push(i);
-  } else {
-    for (let i = start; i >= end; i += step) array.push(i);
-  }
-  return array;
-}
+//   if (step > 0) {
+//     for (let i = start; i <= end; i += step) array.push(i);
+//   } else {
+//     for (let i = start; i >= end; i += step) array.push(i);
+//   }
+//   return array;
+// }
 
-function sum(array) {
-  let total = 0;
-  for (let value of array) {
-    total += value;
-  }
-  return total;
-}
-console.log(range(1, 10));
-console.log(range(1, 10, 2));
-console.log(range(5, 2, -1));
-console.log(sum(range(1, 10)));
+// function sum(array) {
+//   let total = 0;
+//   for (let value of array) {
+//     total += value;
+//   }
+//   return total;
+// }
+// console.log(range(1, 10));
+// console.log(range(1, 10, 2));
+// console.log(range(5, 2, -1));
+// console.log(sum(range(1, 10)));
