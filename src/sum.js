bc89a8078e5bae35d9b5ddc49795f3fc2ebb825a
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-10-31 20:10:35 +0200

    done

--- src/sum.js
+++ src/sum.js
@@ -27,27 +27,27 @@
 
 // console.log(sum(range(1, 10)));
 
-//ex.1 solution 2
+// ex.1 solution 2
 
-// function range(start, end, step = start < end ? 1 : -1) {
-//   let array = [];
+function range(start, end, step = start < end ? 1 : -1) {
+  let array = [];
 
-//   if (step > 0) {
-//     for (let i = start; i <= end; i += step) array.push(i);
-//   } else {
-//     for (let i = start; i >= end; i += step) array.push(i);
-//   }
-//   return array;
-// }
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
+  }
+  return array;
+}
 
-// function sum(array) {
-//   let total = 0;
-//   for (let value of array) {
-//     total += value;
-//   }
-//   return total;
-// }
-// console.log(range(1, 10));
-// console.log(range(1, 10, 2));
-// console.log(range(5, 2, -1));
-// console.log(sum(range(1, 10)));
+function sum(array) {
+  let total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total;
+}
+console.log(range(1, 10));
+console.log(range(1, 10, 2));
+console.log(range(5, 2, -1));
+console.log(sum(range(1, 10)));
